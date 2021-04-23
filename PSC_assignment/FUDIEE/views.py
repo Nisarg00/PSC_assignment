0f3f2a0c3cf6a145e4a76a825cc52e659b60f8b8
Nisarg <patelnjisarg118@gmail.com>
2021-04-23 22:10:53 +0530

    done with featured part and deleted registration, faltu

--- PSC_assignment/FUDIEE/views.py
+++ PSC_assignment/FUDIEE/views.py
@@ -1,40 +1,20 @@
 from  django.shortcuts import render
-from .models import food_item
+from .models import *
 from django.conf import settings
 
 # Create your views here.
-
-this_location_restaurants = [
-    {
-        'Name' : 'riveta',
-        'Rcode': 'R01',
-        'Rating': 4.3,
-    },
-    {
-        'Name' : 'riveta',
-        'Rcode': 'R01',
-        'Rating': 4.3,
-    },
-    {
-        'Name' : 'riveta',
-        'Rcode': 'R01',
-        'Rating': 4.3,
-    }
-]
-
-def home(request):
-    context = {
-        'this_location_restaurants': this_location_restaurants
-    }
-    return render(request,"home.html",context)
 
 def about(request):
     return render(request,"about.html",{'title':'about Fudiee'})
 
 def index(request):
     items = food_item.objects.all()
-    print(settings.BASE_DIR)
-    print(settings.MEDIA_ROOT)
-
-    types = ['Drinks','Pizzas']
-    return render(request,"index.html",{'items': items,'types':types})
+    types = item_types.objects.all()
+    dish = featured_dishes.objects.all()
+    return render(request,"index.html",
+                  {
+                      'items': items,
+                      'types':types,
+                      'dishs':dish
+                  }
+                  )
